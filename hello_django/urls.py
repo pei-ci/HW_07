ac292cc9b21c6ccee70eccf60b9e5d1535131ff9
pei-ci <71923853+pei-ci@users.noreply.github.com>
2021-06-02 17:10:22 +0800

    eleven

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 
 urlpatterns += [
-    path('myhello/',include('myhello.urls')),
+    path(r'',include('myhello.urls')),
+    path(r'', admin.site.urls),
+    #path('myhello/',include('myhello.urls')),
+    #path('admin/', admin.site.urls),
 ]
